d43f9dbba7e8d24f711d8e4c7e881faf91372cf9
KatiaLosignore <katialos.kl@gmail.com>
2022-12-10 15:46:51 +0100

    added bonus

--- js/script.js
+++ js/script.js
@@ -1,30 +1,53 @@
 console.log('JS OK');
 
+// Prendo gli elementi dal DOM
+
+const numberElement = document.getElementById('number');
+
+
+
+
+
 // Creo un ciclo da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-    
+    const div = document.createElement('div');
+    div.className = "col";
+
 
     let finalResult = i;
-   
+
     // Numero divisibile per 3
-    if (i % 3 === 0 && i % 5 > 0){
-        finalResult = 'Fizz';    
+    if (i % 3 === 0 && i % 5 > 0) {
+        finalResult = 'Fizz';
+        div.className += " bg-success";
     }
 
     // Numero divisivile per 5
-    if (i % 5 === 0 && i % 3 > 0){
-        finalResult = 'Buzz';    
+    if (i % 5 === 0 && i % 3 > 0) {
+        finalResult = 'Buzz';
+        div.className += " bg-warning";
+
     }
 
     // Numero divisibile per 3 e 5
-    if (i % 3 === 0 && i % 5 === 0){
-        finalResult = 'FizzBuzz';    
+    if (i % 3 === 0 && i % 5 === 0) {
+        finalResult = 'FizzBuzz';
+        div.className += " bg-danger";
     }
 
+    if (!isNaN(finalResult)) {
+        div.className += " bg-secondary";
+    }
+
+    div.append(finalResult);
+    numberElement.append(div);
 
 
-    console.log(finalResult);
 }
+
+
+console.log(numberElement);
+
 
 
